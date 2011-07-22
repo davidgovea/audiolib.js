c7873e5056a37adde3d75ac46c37bc52adcb90d2
David Govea <govea.d@gmail.com>
2011-07-21 17:00:40 -0700

    little more on the tracker

--- js/generation/tracker.js
+++ js/generation/tracker.js
@@ -6,26 +6,40 @@
  * @param {Number} sampleRate The samplerate to operate the Oscillator on.
  * @param {Number} noteLength The note length in milliseconds
 */
-function Tracker(sampleRate, noteLength, noteCallback){
+function Tracker(sampleRate, channelCount, noteLength, noteCallback){
 	var	self	= this;
 	self.notes	= [];
 	self.sampleRate	= sampleRate;
 	self.noteLength	= isNaN(noteLength) ? 250 : noteLength;
+	self.len = sampleRate * 0.01 * self.noteLength;
+	self.zero = new Float32Array(len);
 }
 
 Tracker.prototype = {
+	
+	_p: 0,
+	
 	generate: function(){
-
+		(this._p += 1) === this.len && this.notes.shift() && (this._p = 0);
 	},
 	
 	getMix: function(){
-		
+		var note = this.notes[0] || this.zero;
+		return note[this._p];
 	},
 	
 /**
  * Adds a Note to be played by the Tracker
 */
 	addNote: function(generateNote){
-		
+		var self = this,
+			l = self.len,
+			note = new Float32Array(l),
+			i;
+			
+		for (i=0; i<l; i++){
+			note[i] = generateNote(self.sampleRate);
+		}
+		self.notes.push(note);
 	},
 }
